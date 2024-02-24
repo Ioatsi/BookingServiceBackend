f2766e76f66d9233e9105b33dcb182aa10316c27
Ioannis Tsilikas <tsilikas@arx.gr>
2024-02-24 17:03:03 +0200

    create booking endpoint

--- app/Http/Controllers/bookingController.php
+++ app/Http/Controllers/bookingController.php
@@ -14,4 +14,26 @@ class bookingController extends Controller
         $bookings = Booking::all(); // Example: Fetch all bookings from the database
         return response()->json($bookings);
     }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'group_id' => 'nullable|exists:groups,id',
+            'recurring_id' => 'nullable|exists:recurrings,id',
+            'booker_id' => 'required|exists:users,id',
+            'title' => 'required',
+            'info' => 'required',
+            'start' => 'required|date',
+            'end' => 'required|date',
+            'semester_id' => 'required|exists:semesters,id',
+            'room_id' => 'required|exists:rooms,id',
+            'status' => 'required|in:0,1',
+            'color' => 'required',
+            'participants' => 'required',
+            'type' => 'required',
+
+        ]);
+        $booking = Booking::create($validatedData);
+        return response()->json(['message' => 'Booking created successfully.', 'booking' => $booking], 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();    
 });
 Route::get('/bookings', [BookingController::class, 'index']);
+Route::post('/createBooking', [BookingController::class, 'store']);
