9091ac4612df1b0e2102045eb2c9ebebffeb630e
Ioannis Tsilikas <tsilikas@arx.gr>
2024-04-17 19:50:54 +0300

    add log

--- app/Http/Middleware/CustomCorsMiddleware.php
+++ app/Http/Middleware/CustomCorsMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class CustomCorsMiddleware
@@ -15,6 +16,8 @@ class CustomCorsMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        
+        Log::info('Req'.$request);
         if ($request->isMethod('OPTIONS')) {
             $response = response('', 200);
 
