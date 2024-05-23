5db2ec37f6d576de7ff61cd59604785b11b82472
Ioatsi <giannischil@gmail.com>
2024-05-23 15:29:32 +0300

    adjust

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -11,39 +11,6 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-
-    public function login(Request $request)
-    {
-        // Validate request
-        $request->validate([
-            'username' => 'required',
-            'password' => 'required',
-        ]);
-
-        // Authenticate user against CAS server
-        $response = Http::post('https://sso.ihu.gr/', [
-            'username' => $request->username,
-            'password' => $request->password,
-        ]);
-
-
-        // Check if the CAS authentication was successful
-        if ($response->successful()) {
-            // Extract the user information or token from the response
-            $userData = $response->json();
-
-            // Optionally, you can store user information in session or database
-            // Example:
-            // session(['user' => $userData]);
-
-            // Return a JSON response indicating successful authentication
-            return response()->json(['message' => 'Authentication successful', 'user' => $userData], 200);
-        } else {
-            // CAS authentication failed
-            // Return a JSON response with error message
-            return response()->json(['error' => 'Invalid username or password'], 401);
-        }
-    }
     public function handleCasCallback(Request $request)
     {
         // Extract the ticket parameter from the request
