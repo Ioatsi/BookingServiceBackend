a4813b22cf97008ed7bee5e0184663f2f755a3fe
Ioannis Tsilikas <tsilikas@arx.gr>
2024-05-26 16:35:41 +0300

    adj

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -13,7 +13,8 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function login(){
+    public function login()
+    {
         $casLoginUrl = config('https://sso.ihu.gr') . '/' . config('login') . '?service=' . urlencode(route('cas.callback'));
         return redirect($casLoginUrl);
     }
@@ -31,20 +32,27 @@ class LoginController extends Controller
             ];
 
             $client = new Client();
-            $response = $client->request('GET', $casValidateUrl, ['query' => $params]);
+            $response = $client->request('GET', $casValidateUrl, ['query' => $params, 'headers' => [
+                'Accept' => 'application/json'
+            ]]);
+
+            $jsonResponse = $response->getBody()->getContents();
+            $data = json_decode($jsonResponse, true); // Decode JSON into associative array
 
             // Parse CAS server response
-            $xmlResponse = $response->getBody()->getContents();
+            /* $xmlResponse = $response->getBody()->getContents();
             $xml = simplexml_load_string($xmlResponse);
-            $xml->registerXPathNamespace('cas', 'http://www.yale.edu/tp/cas');
-            
+            $xml->registerXPathNamespace('cas', 'http://www.yale.edu/tp/cas'); */
+
             // Check if authentication is successful
-            if ($xml && $xml->authenticationSuccess) {
+            if ($data && isset($data['authenticationSuccess'])) {
+                //if ($xml && $xml->authenticationSuccess) {
                 // Extract user attributes
-                $userAttributes = [];
-                foreach ($xml->authenticationSuccess->attributes() as $key => $value) {
+
+                $userAttributes = $data['authenticationSuccess']['attributes'];
+               /*  foreach ($xml->authenticationSuccess->attributes() as $key => $value) {
                     $userAttributes[$key] = (string) $value;
-                }
+                } */
 
                 // Here, you can authenticate the user in your Laravel application using the user attributes.
                 // For example, you can check if the user exists in your database or create a new user.
@@ -61,9 +69,10 @@ class LoginController extends Controller
                     'status' => 'fail',
                     'message' => 'Authentication failed',
                     'redirect_url' => '/',
-                    'xml' => $xml,
+                    'resp' => $response
+                    /* 'xml' => $xml,
                     'xmlResponse' => $xmlResponse,
-                    'path' => $xml->xpath('//cas:authenticationSuccess/cas:user')
+                    'path' => $xml->xpath('//cas:authenticationSuccess/cas:user') */
                 ]);
             }
         } else {
