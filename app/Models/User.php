0447411b1f999947d734919ca5989db9e6555b3b
Ioatsi <giannischil@gmail.com>
2024-05-30 15:41:10 +0300

    adj

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -42,5 +43,13 @@ class User extends Authenticatable
     public function rooms()
     {
         return $this->belongsToMany(Room::class);
+    }
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
