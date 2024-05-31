e45d6078900b27c7bc227478d17cf2ae70948e3e
Ioannis Tsilikas <tsilikas@arx.gr>
2024-05-31 12:38:26 +0300

    add departments

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -22,11 +22,55 @@ class DepartmentSeeder extends Seeder
     public function run(): void
     {
         //
-        Department::create([
-            'name' => 'IEE',
-        ]);
-        Department::create([
-            'name' => 'IEM',
-        ]);
+        $departments = [
+            'LLM in Transnational and European Commercial Law, Banking Law, Arbitration/Mediation',
+            'Master of Arts in Art Law and Arts Management',
+            'MSc in Ε-Business and Digital Marketing',
+            'Μsc in MANAGEMENT',
+            'MSc in Mobile and Web Computing',
+            'Master of Arts in Black Sea and Eastern Mediterranean Studies',
+            'Master of Arts in the Classical Archaeology and the Ancient History of Macedonia',
+            'MSc in Bioeconomy: Biotechnology and Law',
+            'MSc in Cybersecurity',
+            'MSc in Data Science',
+            'MSc in Energy and Finance',
+            'MSc in Energy Building Design',
+            'MSc in Energy Law, Business, Regulation and Policy',
+            'MSc in Energy Systems',
+            'MSc in Environmental Management and Sustainability',
+            'MSc in Hospitality and Tourism Management',
+            'MSc in Information and Communication Technology (ICT) Systems',
+            'MSc in International Accounting, Auditing and Financial Management',
+            'MSc in Strategic Product Design',
+            'ΤΜΗΜΑ ΔΙΟΙΚΗΣΗΣ ΟΡΓΑΝΙΣΜΩΝ, ΜΑΡΚΕΤΙΝΓΚ & ΤΟΥΡΙΣΜΟΥ',
+            'ΤΜΗΜΑ ΑΓΩΓΗΣ ΚΑΙ ΦΡΟΝΤΙΔΑΣ ΣΤΗΝ ΠΡΩΙΜΗ ΠΑΙΔΙΚΗ ΗΛΙΚΙΑ',
+            'ΤΜΗΜΑ ΒΙΒΛΙΟΘΗΚΟΝΟΜΙΑΣ, ΑΡΧΕΙΟΝΟΜΙΑΣ ΚΑΙ ΣΥΣΤΗΜΑΤΩΝ ΠΛΗΡΟΦΟΡΗΣΗΣ',
+            'ΤΜΗΜΑ ΒΙΟΪΑΤΡΙΚΩΝ ΕΠΙΣΤΗΜΩΝ',
+            'ΤΜΗΜΑ ΓΕΩΠΟΝΙΑΣ',
+            'ΤΜΗΜΑ ΕΠΙΣΤΗΜΩΝ ΔΙΑΤΡΟΦΗΣ ΚΑΙ ΔΙΑΙΤΟΛΟΓΙΑΣ',
+            'ΤΜΗΜΑ ΛΟΓΙΣΤΙΚΗΣ ΚΑΙ ΠΛΗΡΟΦΟΡΙΑΚΩΝ ΣΥΣΤΗΜΑΤΩΝ',
+            'ΤΜΗΜΑ ΝΟΣΗΛΕΥΤΙΚΗΣ',
+            'ΤΜΗΜΑ ΦΥΣΙΚΟΘΕΡΑΠΕΙΑΣ',
+            'ΤΜΗΜΑ ΜΗΧΑΝΙΚΩΝ ΠΑΡΑΓΩΓΗΣ ΚΑΙ ΔΙΟΙΚΗΣΗΣ',
+            'ΤΜΗΜΑ ΜΗΧΑΝΙΚΩΝ ΠΕΡΙΒΑΛΛΟΝΤΟΣ ΔΙ.ΠΑ.Ε.',
+            'ΤΜΗΜΑ ΜΗΧΑΝΙΚΩΝ ΠΛΗΡΟΦΟΡΙΚΗΣ ΚΑΙ ΗΛΕΚΤΡΟΝΙΚΩΝ ΣΥΣΤΗΜΑΤΩΝ',
+            'ΤΜΗΜΑ ΕΠΙΣΤΗΜΗΣ ΚΑΙ ΤΕΧΝΟΛΟΓΙΑΣ ΤΡΟΦΙΜΩΝ',
+            'ΤΜΗΜΑ ΜΑΙΕΥΤΙΚΗΣ',
+            'ΤΜΗΜΑ ΟΡΓΑΝΩΣΗΣ ΚΑΙ ΔΙΟΙΚΗΣΗΣ ΕΠΙΧΕΙΡΗΣΕΩΝ',
+            'ΤΜΗΜΑ ΕΣΩΤΕΡΙΚΗΣ ΑΡΧΙΤΕΚΤΟΝΙΚΗΣ',
+            'ΤΜΗΜΑ ΜΗΧΑΝΟΛΟΓΩΝ ΜΗΧΑΝΙΚΩΝ',
+            'ΤΜΗΜΑ ΟΙΚΟΝΟΜΙΚΩΝ ΕΠΙΣΤΗΜΩΝ',
+            'ΤΜΗΜΑ ΜΗΧΑΝΙΚΩΝ ΠΛΗΡΟΦΟΡΙΚΗΣ, ΥΠΟΛΟΓΙΣΤΩΝ ΚΑΙ ΤΗΛΕΠΙΚΟΙΝΩΝΙΩΝ',
+            'ΤΜΗΜΑ ΔΙΟΙΚΗΣΗΣ ΕΦΟΔΙΑΣΤΙΚΗΣ ΑΛΥΣΙΔΑΣ',
+            'ΤΜΗΜΑ ΛΟΓΙΣΤΙΚΗΣ ΚΑΙ ΧΡΗΜΑΤΟΟΙΚΟΝΟΜΙΚΗΣ',
+            'ΤΜΗΜΑ ΜΗΧΑΝΙΚΩΝ ΤΟΠΟΓΡΑΦΙΑΣ ΚΑΙ ΓΕΩΠΛΗΡΟΦΟΡΙΚΗΣ',
+            'ΤΜΗΜΑ ΔΗΜΙΟΥΡΓΙΚΟΥ ΣΧΕΔΙΑΣΜΟΥ ΚΑΙ ΕΝΔΥΣΗΣ',
+            'ΤΜΗΜΑ ΔΙΟΙΚΗΤΙΚΗΣ ΕΠΙΣΤΗΜΗΣ ΚΑΙ ΤΕΧΝΟΛΟΓΙΑΣ',
+            'ΤΜΗΜΑ ΠΟΛΙΤΙΚΩΝ ΜΗΧΑΝΙΚΩΝ'
+        ];
+
+        foreach ($departments as $department) {
+            Department::create(['name' => $department]);
+        }
     }
 }
