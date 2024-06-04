c5d1e52c64d452ba02a9579f3ffe00215fb3fcae
Ioannis Tsilikas <tsilikas@arx.gr>
2024-06-04 13:14:50 +0300

    adj

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -387,14 +387,14 @@ class BookingController extends Controller
             ->pluck('rooms.id')
             ->toArray();
         $roomIds = $request->input('room_id');
-        if ($request->input('room_id') == null) {
-            $roomIds = $allRoomIds;
-        }
         foreach ($userRoles as $role) {
             if ($role->name == 'admin') {
                 $allRoomIds = Room::pluck('rooms.id')
                     ->toArray();
             }
+        }
+        if ($request->input('room_id') == null) {
+            $roomIds = $allRoomIds;
         }
         $semester = Semester::where('is_current', true)->first();
         $query = Booking::join('rooms', 'bookings.room_id', '=', 'rooms.id')
